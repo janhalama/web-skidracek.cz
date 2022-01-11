09be95acb5937edb14f87d2a092f49b530a7c223
Jan Halama <mail@janhalama.cz>
2022-01-11 08:14:01 +0100

    fix: get current temperature from wttr.in instead of api.met.no (api.met.no stopped working)

--- wp-content/themes/dracek/weatherApiClient.php
+++ wp-content/themes/dracek/weatherApiClient.php
@@ -4,17 +4,10 @@ class weatherApiClient
     public function getTemperature($latitude, $longitude, $msl)
     {
         // do something
-		$weatherApiUri = sprintf("https://api.met.no/weatherapi/locationforecast/1.9/?lat=%f&lon=%f&msl=%d", $latitude, $longitude, $msl);
-		//echo('uri: ' . $weatherApiUri);
+		$weatherApiUri = sprintf("https://wttr.in/%f,%f?format=j1", $latitude, $longitude);
 		$response = file_get_contents($weatherApiUri);
-		$xml = simplexml_load_string($response);
-		//echo('xml: ' . $xml->asXML());
-		$time = $xml->xpath('/weatherdata/product/time');
-		//echo('time: ' . $time[0]->asXML());
-		$temp = $time[0]->xpath('location/temperature');
-		//echo('temp: ' . $temp[0]->asXML());
-		//echo('temp value: ' . (string)$temp[0]['value']);
-        return (string)$temp[0]['value'];
+		$weather = json_decode($response);
+        return (string)$weather->current_condition[0]->temp_C;
     }
 }
 ?>
